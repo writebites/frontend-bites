f695420425e8c2755d3b3d8203626a917a8af8a8
Heather Nuffer <zzzuzz@gmail.com>
2020-08-28 16:44:23 -0600

    Style: Adds form styles and some media queries

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import typewriter from '../images/typewriter.jpg';
 
 export default function Register(props) {
   const [newUser, setNewUser] = useState({
@@ -25,7 +26,7 @@ export default function Register(props) {
         .then((response) => {
           localStorage.setItem('token', response.data.token);
           localStorage.setItem('username', newUser.username);
-          props.history.push('/login');
+          props.history.push('/aboutme');
           /*
           // To get the information from the token payload:
           const base64 = response.data.token.split('.')[1];
@@ -45,47 +46,55 @@ export default function Register(props) {
 
   return (
     <div className="register-page">
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="input-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            value={newUser.username}
-            onChange={handleChange}
-            required
-          />
+      <div className="left-side">
+        <div className="image-container">
+          <img src={typewriter} alt="typewriter" />
         </div>
+      </div>
+      <div className="right-side">
+        <h1>Register</h1>
+        <h3>Sign up for a free account today.</h3>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={newUser.username}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-        <div className="input-group">
-          <label htmlFor="password1">Password:</label>
-          <input
-            type="password"
-            name="password1"
-            id="password1"
-            value={newUser.password1}
-            onChange={handleChange}
-            required
-          />
-        </div>
+          <div className="input-group">
+            <label htmlFor="password1">Password:</label>
+            <input
+              type="password"
+              name="password1"
+              id="password1"
+              value={newUser.password1}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-        <div className="input-group">
-          <label htmlFor="password2">Confirm Password:</label>
-          <input
-            type="password"
-            name="password2"
-            id="password2"
-            value={newUser.password2}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        {message && <p>{message}</p>}
+          <div className="input-group">
+            <label htmlFor="password2">Confirm Password:</label>
+            <input
+              type="password"
+              name="password2"
+              id="password2"
+              value={newUser.password2}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          {message && <p>{message}</p>}
 
-        <button type="submit">Submit</button>
-      </form>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
