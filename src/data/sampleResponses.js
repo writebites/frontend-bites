9e4c8affd2d53044e40fb559453232adf58a7bcc
Heather Nuffer <zzzuzz@gmail.com>
2020-09-01 14:17:58 -0600

    Feat: Adds sample writing responses to use for now

---
+++ src/data/sampleResponses.js
@@ -0,0 +1,25 @@
+const sampleResponses = [
+  {
+    prompt: 'A dark and misty night.',
+    writingResponse:
+      'The odor of the disinfectant spray reassured me that the people here cared about cleanliness. While I waited, I read the text on the can nearby: "Lysol Disinfectant Sprays kills 99.9% of the viruses and bacteria* your family comes in contact with every day. Our disinfectants can be used to eliminate germs on commonly touched hard and soft surfaces. This spray eliminates odor causing bacteria and kills mildew, mold, viruses on most household surfaces. EPA #777-99 *Use as directed **Kills Rhinovirus Type 39 and Influenza A Virus (H1N1) on hard, non-porous surfaces in 30 seconds." Definitely reassuring.',
+    promptId: 0,
+    title: 'Droplets of Defense',
+  },
+  {
+    prompt: 'A trail edged by tangled briar.',
+    writingResponse:
+      'I love, love, love blackberries. Fresh, in cobbler, in smoothies, maybe even on pizza. But why oh why do their bushes have to be so thorny? My scars are not battle wounds per se, but testaments to my dedication to gather these berries, despite the tears and rips in my flesh.',
+    promptId: 1,
+    title: 'Paying the Price',
+  },
+  {
+    prompt: 'A young man with a briefcase and cracked glasses.',
+    writingResponse:
+      'Not all young men with broken glasses and a scar on their faces are named Harry Potter. My name is Hairless Dotter, and I also have a story to tell. Let me show you what I have in my briefcase.',
+    promptId: 3,
+    title: 'Glasses and Classes',
+  },
+];
+
+export default sampleResponses;
